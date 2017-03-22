cc0f21890597a13eee9015950da8849327de76c8
Emerson Jr <emerson_jdss@hotmail.com>
2017-03-22 07:31:47 -0300

    JS Prologue

--- aluraframe/client/js/index.js
+++ aluraframe/client/js/index.js
@@ -1,14 +1,14 @@
 var campos = [
-    document.querySelector("#data"),
-    document.querySelector("#quantidade"),
-    document.querySelector("valor")
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('valor')
 ];
 
 var tbody = document.querySelector('table tbody');
 
 document.querySelector('.form').addEventListener('submit', function(e) {
 
-    e.preventDefault()
+    e.preventDefault();
     var tr = document.createElement('tr');
 
     campos.forEach(function(campo){
@@ -23,6 +23,13 @@ document.querySelector('.form').addEventListener('submit', function(e) {
     tr.appendChild(tdVolume);
 
     tbody.appendChild(tr);
+
+    campos[0].value = " ";
+    campos[1].value = 1;
+    campos[2].value = 0;
+
+    campos[0].focus();
+
 
 });
 
