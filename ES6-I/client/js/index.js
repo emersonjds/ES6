07b051225cb34c85f802ee8c54fa6cecac3b4261
Emerson Jr <emerson_jdss@hotmail.com>
2017-05-04 20:34:06 -0300

    Working with OO paradigm in ES6

--- ES6-I/client/js/index.js
+++ ES6-I/client/js/index.js
@@ -1,7 +1,35 @@
-var campos = [
+let campos = [
   document.querySelector('#data'),
   document.querySelector('#quantidade'),
   document.querySelector('#valor')
 ];
 
-console.log(campos);
+document.querySelector('.form').addEventListener('submit', (e) => {
+
+  e.preventDefault();
+
+  let tbody = document.querySelector('table tbody')
+
+  let tr = document.createElement('tr')
+
+  campos.forEach((campo) => {
+
+    let td = document.createElement('td')
+    td.textContent = campo.value
+    tr.appendChild(td)
+  })
+
+  let tdVolume = document.createElement('td')
+  tdVolume.textContent = campos[1].value * campos[2].value
+
+  tr.appendChild(tdVolume)
+
+  tbody.appendChild(tr)
+
+  //setando valores para um melhor UX
+  campos[0].value = ''
+  campos[1].value = 1
+  campos[2].value = ''
+
+  campos[0].focus()
+})
