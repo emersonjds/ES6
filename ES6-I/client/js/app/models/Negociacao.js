4f7c8bc5b791b0d7bed1639d24c0c2804696b07c
Emerson Jr <emerson_jdss@hotmail.com>
2017-05-04 21:51:42 -0300

    Working with intern properties and imutability

--- ES6-I/client/js/app/models/Negociacao.js
+++ ES6-I/client/js/app/models/Negociacao.js
@@ -1,24 +1,28 @@
 class Negociacao {
 
   constructor(data, quantidade, valor) {
-    this._data = data
+    this._data = new Date(data.getTime())
     this._quantidade = quantidade
     this._valor = valor
+    Object.freeze(this) //congelando objeto para nao permitir alteração de propriedades com uma nova instancia
   }
 
-  obtemVolume() {
-    return this._quantidade * this._valor;
+  //criacao de acesso getter 
+  // a utilização desta propriedade nao permite que o atributo seja setado utilizando a chamada convencional
+  get volume() {
+    return this._quantidade * this._valor
   }
   
-  getData() {
-    return this._data
+  get data() {
+    return new Date(this._data.getTime())
+    // o object.freeze nao funciona em um outro objeto data, desta forma é necessario utilizar esta opção 
   }
 
-  getQuantidade() {
+  get quantidade() {
     return this._quantidade
   }
 
-  getValor() {
+  get valor() {
     return this._valor
   }
 
