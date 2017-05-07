5720f8779b0be8fc4912278fa422a34e9bc559d1
Emerson Jr <emerson_jdss@hotmail.com>
2017-05-07 14:56:27 -0300

    Working with intern functions on Controller

--- ES6-I/client/js/app/controllers/NegociacaoController.js
+++ ES6-I/client/js/app/controllers/NegociacaoController.js
@@ -1,11 +1,37 @@
 class NegociacaoController {
-    
-    adiciona(event) {
-        event.preventDefault()
-        alert('Chamei ação no Controller')
+
+    constructor() {
+        //o bind mantem a associação de contexto do document ao metoto querySelector
+        let $ = document.querySelector.bind(document)
+
+        this._inputData = $('#data')
+        this._inputQuantidade = $('#quantidade')
+        this._inputValor = $('#valor')
     }
 
-    
+    adiciona(event) {
+        event.preventDefault()
+
+        // ". . . " 
+        //utilização do spreed operator, em que ele explicita que cada item do array vai entrar com o posicionamento direto 
+        let data = new Date(...
+            this._inputData.value
+                .split('-')
+                .map((item, indice) => item - indice % 2)
+                //af com instrução unica ja é auto retornavel
+        )
+
+        let negociacao = new Negociacao (
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
+
+        console.log(negociacao)
+
+    }
+
+
 
 
 }
