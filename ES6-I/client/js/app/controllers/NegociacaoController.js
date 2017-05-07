b3b827ddda16e59505378a6fcfab4c9e84aa9ea8
Emerson Jr <emerson_jdss@hotmail.com>
2017-05-07 15:40:38 -0300

    working with functional programming

--- ES6-I/client/js/app/controllers/NegociacaoController.js
+++ ES6-I/client/js/app/controllers/NegociacaoController.js
@@ -18,10 +18,10 @@ class NegociacaoController {
             this._inputData.value
                 .split('-')
                 .map((item, indice) => item - indice % 2)
-                //af com instrução unica ja é auto retornavel
+            //af com instrução unica ja é auto retornavel
         )
 
-        let negociacao = new Negociacao (
+        let negociacao = new Negociacao(
             data,
             this._inputQuantidade.value,
             this._inputValor.value
@@ -29,9 +29,22 @@ class NegociacaoController {
 
         console.log(negociacao)
 
+
+        //Exercices 
+        let dataString = '17-05-2016'
+        dataString = dataString.split('-').reverse().join('/')
+
+        let numeros = [3, 2, 11, 20, 8, 7]
+
+        numeros.map((item) => {
+            if (item % 2 != 0)
+                return item * 2
+            return item
+        })
+
+        //Refactor
+        numeros.map((item) => item % 2 != 0 ? item * 2 : item)
+        
+
     }
-
-
-
-
 }
