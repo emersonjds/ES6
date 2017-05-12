7a2de7ab317d5ae82fd2339b23d5d4dfddf52fc2
Emerson Jr <emerson_jdss@hotmail.com>
2017-05-12 17:19:39 -0300

    Intern Pattern Proxy in JS

--- ES6-II/client/js/app/controllers/NegociacaoController.js
+++ ES6-II/client/js/app/controllers/NegociacaoController.js
@@ -7,7 +7,27 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade')
         this._inputValor = $('#valor')
 
-        
+        let self = this
+
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+            //target -> objeto original que esta sendo acessado
+            //prop -> propriedade do objeto que esta sendo acessada 
+            //receiver -> referencia para o próprio Proxy
+
+            //o get sera chamado sempre que uma propriedade for acessada
+            get(target, prop, receiver) {
+                if (['adiciona', 'esvazia'].includes(prop) && typeof (target[prop]) == typeof (Function)) {
+                    //includes, verifica se a propriedade existe no array ou nao, retornando booleano
+                    return function() {
+                        Reflect.apply(target[prop], target, arguments)
+                        self._negociacoesView.update(target)
+                    }
+                }
+                return Reflect.get(target, prop, receiver)
+                // //chamada da API de reflect
+            }
+        })
+
         //af mantem o contexto léxico do código diferente da utilização de function normal o que torna o contexto nao mais dinamico
         // this._listaNegociacoes = new ListaNegociacoes(model =>
         //     this._negociacoesView.update(model)
@@ -15,8 +35,6 @@ class NegociacaoController {
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'))
         this._negociacoesView.update(this._listaNegociacoes)
-
-
 
         this._mensagem = new Mensagem()
         this._mensagemView = new MensagemView($('#mensagemView'))
