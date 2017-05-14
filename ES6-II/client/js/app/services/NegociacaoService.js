39fce7a4b12b543d2591ad3db495de0904f8a258
Emerson Jr <emerson_jdss@hotmail.com>
2017-05-13 22:23:28 -0300

    Working with Callback Hell

--- ES6-II/client/js/app/services/NegociacaoService.js
+++ ES6-II/client/js/app/services/NegociacaoService.js
@@ -19,5 +19,42 @@ class NegociacaoService {
     xhr.send()
   }
 
-  
+  obterNegociacoesDaSemanaRetrasada(callback) {
+    let xhr = new XMLHttpRequest()
+
+    xhr.open('GET', 'negociacoes/retrasada')
+    //config
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          callback(null, JSON.parse(xhr.responseText)
+            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)))
+        } else {
+          console.log(xhr.responseText)
+          callback('Nao foi possivel receber as negociacoes', null)
+        }
+      }
+    }
+    xhr.send()
+  }
+
+  obterNegociacoesDaSemanaAnterior(callback) {
+    let xhr = new XMLHttpRequest()
+
+    xhr.open('GET', 'negociacoes/anterior')
+    //config
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          callback(null, JSON.parse(xhr.responseText)
+            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)))
+        } else {
+          console.log(xhr.responseText)
+          callback('Nao foi possivel receber as negociacoes', null)
+        }
+      }
+    }
+    xhr.send()
+  }
+
 }
