19a66a8939903f0afe848a5d0bc4737c4538a393
Emerson Jr <emerson_jdss@hotmail.com>
2017-05-19 23:42:51 -0300

    Working with best practices in Promise returns

--- ES6-III/client/js/app/controllers/NegociacaoController.js
+++ ES6-III/client/js/app/controllers/NegociacaoController.js
@@ -18,6 +18,19 @@ class NegociacaoController {
             'texto');
 
         this._ordemAtual = ''
+
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection)) //return implicito com a af
+            .then(dao => dao.listaTodos())
+            .then(negociacoes =>
+                negociacoes.forEach(negociacao =>
+                    this._listaNegociacoes.adiciona(negociacao)))
+            .catch(erro => {
+                console.log(erro)
+                this._mensagem.texto = erro
+            })
+
     }
 
     adiciona(event) {
@@ -53,9 +66,14 @@ class NegociacaoController {
     }
 
     apaga() {
-
-        this._listaNegociacoes.esvazia();
-        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.apagaTodos())
+            .then(mensagem => {
+                this._mensagem.texto = mensagem
+                this._listaNegociacoes.esvazia()
+            })
     }
 
     _criaNegociacao() {
--- ES6-III/client/js/app/dao/NegociacaoDao.js
+++ ES6-III/client/js/app/dao/NegociacaoDao.js
@@ -29,4 +29,52 @@ class NegociacaoDao {
 
     });
   }
+
+  listaTodos() {
+
+    return new Promise((resolve, reject) => {
+
+      let cursor = this._connection
+        .transaction([this._store], 'readwrite')
+        .objectStore(this._store)
+        .openCursor()
+
+      let negociacoes = []
+
+      cursor.onsuccess = e => {
+        let atual = e.target.result
+
+        if (atual) {
+          let dado = atual.value
+
+          negociacoes.push(new Negociacao(dado._data, dado._quantidade, dado._valor))
+
+          atual.continue()
+        } else {
+          resolve(negociacoes)
+        }
+      }
+
+      cursor.onerror = e => {
+        consoele.log(e.target.error)
+        reject('Nao foi possivel listas as negociacoes ')
+      }
+    })
+  }
+
+  apagaTodos() {
+    return new Promise((resolve, reject) => {
+      let request = this._connection
+        .transaction([this._store], 'readwrite')
+        .objectStore(this._store)
+        .clear()
+
+      request.onsuccess = e => resolve('Negocicaes removidas com sucesso')
+      request.onerror = e => {
+        console.log(e.target.error)
+        reject('Nao foi possivel remover as negociacoes')
+      }
+    })
+  }
+
 }
