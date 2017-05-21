743b175c9c160f6c693cc11c1ccd081a9d79b9b8
Emerson Jr <emerson_jdss@hotmail.com>
2017-05-21 00:56:54 -0300

    Working with Fetch API

--- ES6-III/client/js/app/services/HttpService.js
+++ ES6-III/client/js/app/services/HttpService.js
@@ -1,57 +1,31 @@
 class HttpService {
-    
-    get(url) {
-        
-        return new Promise((resolve, reject) => {
-            
 
-            let xhr = new XMLHttpRequest();
-            
-            xhr.open('GET', url);
-            
+    _handleErrors(res) {
+        if (!res.ok) throw new Error(res.statusText)
+        return res
+    }
+
+    get(url) {
+        return fetch(url) // API fetch retorna uma promise
+            .then(res => this._handleErrors(res))
+            .then(res => res.json()) // o retorno ja consegue ser parseado
+    }
+
+    post(url, dado) {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest()
+            xhr.open("POST", url, true)
+            xhr.setRequestHeader("Content-type", "application/json")
             xhr.onreadystatechange = () => {
-                    
-                if(xhr.readyState == 4) {
-                    
-                    if(xhr.status == 200) {   
-                        
-                        resolve(JSON.parse(xhr.responseText));  
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        resolve(JSON.parse(xhr.responseText))
                     } else {
-                        
-                        reject(xhr.responseText);
+                        reject(xhr.responseText)
                     }
                 }
-            };
-            
-            xhr.send();
-             
-            
+            }
+            xhr.send(JSON.stringify(dado)) // usando JSON.stringifly para converter objeto em uma string no formato JSON.
         });
     }
-    
-    post(url, dado) {
-
-
-            return new Promise((resolve, reject) => {
-
-                let xhr = new XMLHttpRequest();
-                xhr.open("POST", url, true);
-                xhr.setRequestHeader("Content-type", "application/json");
-                xhr.onreadystatechange = () => {
-
-                    if (xhr.readyState == 4) {
-
-                        if (xhr.status == 200) {
-
-                            resolve(JSON.parse(xhr.responseText));
-                        } else {
-
-                            reject(xhr.responseText);
-                        }
-                    }
-                };
-                xhr.send(JSON.stringify(dado)); // usando JSON.stringifly para converter objeto em uma string no formato JSON.
-            });
-
-        }
 }
